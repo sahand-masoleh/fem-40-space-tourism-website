c88035783cce2b7a46406a0819f61255921cb7b5
Sahand <sahand.masoleh@gmail.com>
2022-09-16 22:34:04 +0200

    [feat] nav: mobile
    [style] nav: mobile

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,8 +2,11 @@
 
 import "./Nav.scss";
 
+import React, { useState } from "react";
 import { ReactComponent as Logo } from "@assets/shared/logo.svg";
 import HoverMenu, { menuItem } from "@components/HoverMenu/HoverMenu";
+import { ReactComponent as OpenIcon } from "@assets/shared/icon-hamburger.svg";
+import { ReactComponent as CloseIcon } from "@assets/shared/icon-close.svg";
 
 export enum paths {
 	home = "home",
@@ -27,13 +30,33 @@ for (let index = 0; index < Object.keys(paths).length; index++) {
 }
 
 function Nav() {
+	const [isOpen, setIsOpen] = useState<boolean>(true);
+
+	function handleMobileMenu(): void {
+		setIsOpen((prev) => !prev);
+	}
+
 	return (
-		<nav className="nav">
+		<nav
+			className="nav"
+			style={{ "--menu-open": isOpen ? 1 : "" } as React.CSSProperties}
+			onClick={isOpen ? handleMobileMenu : undefined}
+		>
 			<div className="nav__logo image">
 				<Logo className="image__img" />
 			</div>
 			<div className="nav__line"></div>
 			<HoverMenu block="nav" items={items} />
+			<button
+				className="nav__button image"
+				onClick={!isOpen ? handleMobileMenu : undefined}
+			>
+				{!isOpen ? (
+					<OpenIcon className="image__img" />
+				) : (
+					<CloseIcon className="image__img" />
+				)}
+			</button>
 		</nav>
 	);
 }
